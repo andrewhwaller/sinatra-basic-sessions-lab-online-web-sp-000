a51c60f0a7aac0ccf63f70ac9c5a935a79bf6f62
Andrew Waller <andrewhwaller@gmail.com>
2019-04-24 03:05:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,8 +11,9 @@ class App < Sinatra::Base
     erb :index
   end
 
-  post '/checkout'
+  post '/checkout' do
     session[:item] = params[:item]
     @session = session
     erb :checkout
+  end
 end
